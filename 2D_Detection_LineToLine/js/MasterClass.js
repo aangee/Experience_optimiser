266fb03d3143cf0dede40496ede8e3d3052458fa
aangee <gohanss4@hotmail.fr>
2022-04-17 22:22:46 +0200

    Test master class

---
+++ 2D_Detection_LineToLine/js/MasterClass.js
@@ -0,0 +1,23 @@
+class MasterClass{
+    constructor(){}
+    
+    Vec2d(x, y){
+        
+    }
+
+    Vec = (x, y)=>{
+        
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+    /*class Point{
+
+    }
+    class Line{}
+
+    class Particle{}*/
+}
+
+let taaaaa = MasterClass.Vec(4,9);
+console.log( taaaaa.x);

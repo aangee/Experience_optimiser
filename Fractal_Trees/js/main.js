499dbdf149835b56712e161b9a78d6747104682e
aangee <gohanss4@hotmail.fr>
2022-04-18 13:44:54 +0200

    test function via btn

--- Fractal_Trees/js/main.js
+++ Fractal_Trees/js/main.js
@@ -6,15 +6,24 @@ window.onload = _ => {
 	let nbsFonctionScript = ['f_Ifs', 'f_PyTree', 'f_PyTree_Anim', 'f_Tree', 'f_Tree_Anim'];
 
 	for (let i = 0; i < nbsFonctionScript.length; i++) {
-		let nameFunc = nbsFonctionScript[i].valueOf();
+		let nameFunc = nbsFonctionScript[i];
 		let btn = document.createElement('button');
-		btn.addEventListener('click', () => { `${nameFunc}`() });
+		btn.addEventListener('click', ()=>{clickSwitch(nameFunc)});
 		btn.textContent = nameFunc;
 		container.appendChild(btn);
+
+		/*var GeneratorFunction = Object.getPrototypeOf(function*(){}).constructor;
+var g = new GeneratorFunction("a", "yield a * 2");
+var itérateur = g(10);
+console.log(itérateur.next().value); // 20*/
+
 	}
 
-	function clickSwitch() {
-		console.log('hi');
+	function clickSwitch(name) {
+		
+		name();
+
+		console.debug(name,name());
 	}
 
 

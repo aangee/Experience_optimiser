0e78135de5b61ac45b0b7692f39ea1354549d71b
aangee <gohanss4@hotmail.fr>
2022-04-18 23:30:20 +0200

    maj

---
+++ @_Paticule/2_2D_Simple_Gravity/js/main.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+    var canvas = document.getElementById('canvas'),
+        context = canvas.getContext("2d"),
+        width = canvas.width = window.innerWidth,
+        height = canvas.height = window.innerHeight,
+        sun = particle.create(width / 2, height / 2, 0, 0),
+        planet = particle.create(width / 2 + 200, height / 2, 10, -M_PI / 2),
+        accel = vector.create(0.1, 0.1);
+
+    sun.mass = 20000;
+
+    update();
+    function update() {
+        context.clearRect(0, 0, width, height);
+
+        planet.gravitateTo(sun);
+        planet.update();
+
+        context.beginPath();
+        context.fillStyle = '#ffff00';
+        context.arc(sun.position.getX(), sun.position.getY(), 20, 0, M_PI * 2, false);
+        context.fill();
+
+        context.beginPath();
+        context.fillStyle = '#00ffff';
+        context.arc(planet.position.getX(), planet.position.getY(), 5, 0, M_PI * 2, false);
+        context.fill();
+
+        requestAnimationFrame(update);
+    }
+}

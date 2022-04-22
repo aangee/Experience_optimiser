ede0f084a68ce7eef6b378e662e43d5ab510999d
aangee <gohanss4@hotmail.fr>
2022-04-22 23:22:38 +0200

    maj project

--- @_Detection/2D_Detection_Bitmap/MyTest01.js
+++ @_Detection/2D_Detection_Bitmap/MyTest01.js
@@ -1,16 +1,9 @@
-
-const M_PI = Math.PI;
-
-//#region Debug
-//Div pour le debug pos sourie
-let DEBUG_ELEMENT = document.getElementById('js-debug');
-let DEBUG_COLOR = 'rgba(150, 25, 75, 0)';
-//#endregion
-//Juste pour le debug
+// & Juste pour le debug
 // u = update // ^ le nbs de foi ou detecte rien
 // a = ajoute  // ^ le nbs de foi ou ajoute de la couleur en utilisent 'globalCompositeOperation' en mode = 'destination-over'
 // r = retire  // ^ le nbs de foi ou retire de la couleur en utilisent 'globalCompositeOperation' en mode = 'destination-out'
-let u = 0, a = 0, r = 0;
+// raz = raz  // ^ le nbs de foi ou une particule et remise a zero
+let u = 0, a = 0, r = 0, raz = 0;
 
 
 window.onload = function () {
@@ -22,15 +15,17 @@ window.onload = function () {
     let height = canvas.height = targetCanvas.height = window.innerHeight;
 
 
-    let p0 = particle.create(0, height / 2, 10, 0);
-    let p1 = particle.create(0, height / 2, 10, 0);
-    let p2 = particle.create(0, height / 2, 10, 0);
-    let p3 = particle.create(0, height / 2, 10, 0);
+    let p0 = particle.create(0, height / 2, 3, utils.randomRange(-.52, .52));
+    p0.isSpawn = false;//& <-- Juste pour le debug
+
+    let p1 = particle.create(0, height / 2, 3, utils.randomRange(-.52, .52));
+    let p2 = particle.create(0, height / 2, 3, utils.randomRange(-.52, .52));
+    let p3 = particle.create(0, height / 2, 3, utils.randomRange(-.52, .52));
 
 
 
     // Ici on dessine une gros boule sur la droite
-    // sais cible pour que l'on puis retire de la couleur 
+    // sais la cible sur la quelle on poura retire de la couleur 
     targetContext.beginPath();
     targetContext.fillStyle = "red";
     targetContext.strokeStyle = 'gold';
@@ -41,14 +36,12 @@ window.onload = function () {
 
 
     update();
-
-
     function update() {
         requestAnimationFrame(update);
         context.clearRect(0, 0, width, height);
 
 
-        drawProjectile({ p: p0, color: 'green' });
+        drawProjectile({ p: p0, color: 'gold' });
         drawProjectile({ p: p1, color: 'red' });
         drawProjectile({ p: p2, color: 'white' });
         drawProjectile({ p: p3, color: 'blue' });
@@ -59,11 +52,16 @@ window.onload = function () {
         getCollisionBitmap(p2, .45);
         getCollisionBitmap(p3, .40);
 
+        if (p0.isSpawn) p0.update();//& <-- Juste pour le debug
+        p1.update();
+        p2.update();
+        p3.update();
 
+        // & Juste pour le debug
         DEBUG_ELEMENT.innerHTML = `
-            <p style='width:60%'>Update :</p><span class='js-spanDeb'>${u}</span>
+            <p style='width:60%'>Retire :</p><span class='js-spanDeb'>${r}</span>
             <p style='width:60%'>Ajouter :</p><span class='js-spanDeb'>${a}</span>
-            <p style='width:60%'>Retirer :</p><span class='js-spanDeb'>${r}</span>`;
+            <p style='width:60%'>Reset :</p><span class='js-spanDeb'>${raz}</span>`;
 
     }
 
@@ -96,13 +94,11 @@ window.onload = function () {
             targetContext.fill();
             //targetContext.stroke();
 
-            a++;
+            a++;//& <-- Juste pour le debug
             resetParticle(_p, _offsetSpawn);
             //resetParticle1();
         }
-
-        // On check le si l'alpha depasse le 0 
-        if (imageData.data[3] > 0) {
+        else if (imageData.data[3] > 0) {// On check le si l'alpha depasse le 0
 
             // Ici on n'a detecter que 'alpha' n'ai plus a 0, alors a l'endroi de la detection on dessin un 'arc' 'green' de rayon 30
             // se qui a pour effet de faire un trou
@@ -112,22 +108,39 @@ window.onload = function () {
             targetContext.arc(_p.x, _p.y, 30, 0, Math.PI * 2, false);
             targetContext.fill();
 
-            r++;
+            r++;//& <-- Juste pour le debug
             resetParticle(_p, _offsetSpawn);
         }
 
-        if (_p.x > width) {
-
-            resetParticle(_p, _offsetSpawn);
-        }
-
-        u++;
-        _p.update();
+        // Si on depasse l taille de notre canvas en (X ou en W) 
+        if (_p.x > width) resetParticle(_p, _offsetSpawn);  
     }
 
     function resetParticle(_p, _offsetSpawn) {
         _p.x = 0;
         _p.y = height * _offsetSpawn;
         _p.setHeading(utils.randomRange(-.52, .52));
+
+
+        if (_p.isSpawn) p0.isSpawn = false;//& <-- Juste pour le debug
+        raz++;//& <-- Juste pour le debug
     }
+    // & Juste pour le debug
+    targetCanvas.addEventListener('click', (e) => {
+
+        if (!p0.isSpawn) {
+            let { layerX: x, layerY: y } = e;
+
+            p0.x = x;
+            p0.y = y;
+            p0.setHeading(utils.randomRange(-.52, .52));
+            p0.setSpeed(5);
+            p0.isSpawn = true;
+        }
+    });
+}
+
+
+function razInfos() {
+    r = a = raz = 0;
 }
